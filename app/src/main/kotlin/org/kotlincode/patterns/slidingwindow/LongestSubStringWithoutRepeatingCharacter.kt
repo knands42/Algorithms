ad977974a341b22cc06202d3f2c9445f30de93de
Caio Fernandes <caiow.wk2@proton.me>
2025-02-10 20:11:36 -0300

    feat: add LongestSubStringWithoutRepeatingCharacter

--- app/src/main/kotlin/org/kotlincode/patterns/slidingwindow/LongestSubStringWithoutRepeatingCharacter.kt
+++ app/src/main/kotlin/org/kotlincode/patterns/slidingwindow/LongestSubStringWithoutRepeatingCharacter.kt
@@ -1,6 +1,27 @@
 package org.kotlincode.patterns.slidingwindow
 
+import kotlin.math.max
+
 class LongestSubStringWithoutRepeatingCharacter {
 
-    fun execute() {}
+    fun execute(s: String): Int {
+        if (s.length == 1) return 1
+
+        var maxSubstringSize = 0
+        val map = mutableMapOf<Char, Int>()
+
+        var window = Pair(0, 1)
+
+        for ((i, char) in s.withIndex()) {
+            if (window.second > s.length) break
+
+            if (map.contains(char)) window = Pair(map[char]!! + 1, window.second)
+
+            maxSubstringSize = max(maxSubstringSize, s.substring(window.first..<window.second).count())
+            window = Pair(window.first, window.second + 1)
+            map[char] = i
+        }
+
+        return maxSubstringSize
+    }
 }
